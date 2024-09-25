ea828f441d00a9b96f89029df20862d498a8d8d4
cameroncooper <c@meron.cc>
2024-09-24 20:48:18 -0500

    added events command (#6)

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 
 require (
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/itchyny/timefmt-go v0.1.4 // indirect
@@ -19,5 +20,6 @@ require (
 	github.com/samber/mo v1.13.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/cmd/coinset/get_additions_and_removals.go
+++ internal/cmd/coinset/get_additions_and_removals.go
@@ -1,31 +1,31 @@
 package cmd
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 func init() {
-    rootCmd.AddCommand(getAdditionsAndRemovalsCmd)
+	rootCmd.AddCommand(getAdditionsAndRemovalsCmd)
 }
 
 var getAdditionsAndRemovalsCmd = &cobra.Command{
-    Use:   "get_additions_and_removals <header_hash>",
-    Args: func(cmd *cobra.Command, args []string) error {
-        if err := cobra.ExactArgs(1)(cmd, args); err != nil {
-            return err
-        }
-        if isHex(args[0]) == true {
-            return nil
-        }
-        return fmt.Errorf("invalid hex value specified: %s", args[0])
-    },
-    Short: "Retrieves the additions and removals for a certain block",
-    Long:  "Retrieves the additions and removals for a certain block. Returns coin records for each addition and removal. Blocks that are not transaction blocks will have empty removal and addition lists. To get the actual puzzles and solutions for spent coins, use the get_puzzle_and_solution api.",
-    Run: func(cmd *cobra.Command, args []string) {
-        jsonData := map[string]interface{}{}
-        jsonData["header_hash"] = formatHex(args[0])
-        makeRequest("get_additions_and_removals", jsonData)
-    },
+	Use: "get_additions_and_removals <header_hash>",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if err := cobra.ExactArgs(1)(cmd, args); err != nil {
+			return err
+		}
+		if isHex(args[0]) {
+			return nil
+		}
+		return fmt.Errorf("invalid hex value specified: %s", args[0])
+	},
+	Short: "Retrieves the additions and removals for a certain block",
+	Long:  "Retrieves the additions and removals for a certain block. Returns coin records for each addition and removal. Blocks that are not transaction blocks will have empty removal and addition lists. To get the actual puzzles and solutions for spent coins, use the get_puzzle_and_solution api.",
+	Run: func(cmd *cobra.Command, args []string) {
+		jsonData := map[string]interface{}{}
+		jsonData["header_hash"] = formatHex(args[0])
+		makeRequest("get_additions_and_removals", jsonData)
+	},
 }
